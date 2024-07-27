65861ebc3ffe70c397c50711951999b44bbed081
QuicoPBernal <quico.hotel@gmail.com>
2024-07-27 19:12:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Considere três notas (n1, n2, n3) e seus respectivos pesos (p1, p2, p3). Calcule a média ponderada das notas e atribua o resultado à variável "media". 
-Após o cálculo, exiba a média ponderada no console. OBS: Para calcular a média ponderada multiplica os valores das notas pelos valores dos pesos, 
-e divide pelas somas de todos os pesos. Lembre-se de declarar valores para os pesos e para as notas.
+// Após o cálculo, exiba a média ponderada no console. OBS: Para calcular a média ponderada multiplica os valores das notas pelos valores dos pesos, 
+// e divide pelas somas de todos os pesos. Lembre-se de declarar valores para os pesos e para as notas.
 
 let n1 = Number(prompt("Digite a primeira nota: "));
 let n2 = Number(prompt("Digite a segunda nota: "));
